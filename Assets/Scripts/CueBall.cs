99fe67a0cb18d3e2ebcddce054501ad962e28f9c
TheeEmuu <chertangoldsmith@gmail.com>
2020-08-31 02:10:50 -0400

    Initial commit

---
+++ Assets/Scripts/CueBall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueBall : MonoBehaviour {
+	public GameObject cueStick;
+	Rigidbody rb;
+	Rigidbody cuerb;
+	
+	float speed;
+	bool flip;
+	Vector3 cueBallPosition;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		cuerb = cueStick.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = rb.velocity.magnitude;
+
+		if (speed == 0 && flip){
+			// rb.velocity = Vector3.zero;
+			cueBallPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 3);
+			// flip = false;
+			// cueStick.transform.position = cueBallPosition;
+			// cueStick.transform.Translate (0,-3,0);
+			// cueStick.SetActive(true);
+			
+			cuerb.velocity = Vector3.zero;
+			flip = false;
+			cueStick.transform.position = cueBallPosition;
+			cueStick.transform.rotation = Quaternion.Euler(90,0,0);
+			cueStick.SetActive(true);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.rigidbody.name == "Cue Stick")
+			flip = true;
+	}
+}
---
+++ Assets/Scripts/CueStick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CueStick : MonoBehaviour {
+	public GameObject cueBall;
+	Vector3 cueBallTransform;
+	Rigidbody rb;
+
+	float r;
+	float v;
+	float deltaDistance;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		r = Input.GetAxis("Mouse X");
+		v = Input.GetAxis("Mouse Y");
+		cueBallTransform = new Vector3(cueBall.transform.position.x, cueBall.transform.position.y, cueBall.transform.position.z);
+		deltaDistance = Vector3.Distance(cueBallTransform, transform.position);
+		float lerp = 3;
+
+		transform.RotateAround (cueBallTransform, new Vector3(0,1,0), r);
+
+
+		if(v < 0)
+			v = Mathf.Lerp(-lerp,0,v);
+		else
+			v = Mathf.Lerp(0,lerp,v);
+		if((deltaDistance < 3 && v > 0) || (deltaDistance > 5 && v < 0))
+			transform.Translate (0,0,0);
+		else{
+			transform.Translate (0, v * Time.deltaTime * 2, 0);
+		}	
+
+		if(Input.GetMouseButtonDown(0)){
+			Charge(deltaDistance);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.rigidbody.name == "Cue Ball"){
+			gameObject.SetActive(false);
+		}	
+	}
+
+	void Charge(float distance){
+		float thrust = Mathf.Pow(distance, 2);
+		rb.AddForce(transform.up * thrust, ForceMode.Impulse);
+	}
+}
